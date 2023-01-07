a5c27be7f2e47193587f6c1494104ed3b1481260
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-01-07 19:05:14 +0500

    add local host

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ## Netflix
+
+ALTER ROLE deployer SET client_encoding TO 'utf8';
+ALTER ROLE deployer SET default_transaction_isolation TO 'read committed';
+ALTER ROLE deployer SET timezone TO 'UTC';
--- netflix/settings.py
+++ netflix/settings.py
@@ -97,7 +97,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DB'), 
         'USER': os.environ.get('POSTGRES_USER'), 
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),    
+        'HOST': os.environ.get('POSTGRES_HOST', '127.0.0.1'),    
         'PORT': os.environ.get('POSTGRES_PORT')
    }
 }
