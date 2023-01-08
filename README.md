30630c597bfa7db1624340b262a1fcb60b9630c9
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-01-08 16:46:27 +0500

    static url

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ ALTER ROLE posrgres SET timezone TO 'UTC';
 
 
 GRANT ALL PRIVILEGES ON DATABASE netflix TO posrgres;
+
+gunicorn --bind 0.0.0.0:8000 netflix.wsgi
+
+sudo ln -s /etc/nginx/sites-available/netflix_nginx /etc/nginx/sites-enabled
--- netflix/urls.py
+++ netflix/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
+from django.conf.urls.static import static
+from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -37,3 +39,4 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc-docs'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
