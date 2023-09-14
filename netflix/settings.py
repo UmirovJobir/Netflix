8015f7ab0f10fc66d2afcbfee4e64e0424a4cf5b
UmirovJobir <jobirumirovmoliya@gmail.com>
2023-09-14 19:09:01 +0500

    requirements without version

--- netflix/settings.py
+++ netflix/settings.py
@@ -83,27 +83,27 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('POSTGRES_DB'), 
-#         'USER': os.environ.get('POSTGRES_USER'), 
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-#         'HOST': os.environ.get('POSTGRES_HOST'),    
-#         'PORT': os.environ.get('POSTGRES_PORT')
-#    }
-# }
-
 DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'netflix', 
-        'USER': 'postgres', 
-        'PASSWORD': '123',
-        'HOST': '127.0.0.1',    
-        'PORT': 5432
+        'NAME': os.environ.get('POSTGRES_DB'), 
+        'USER': os.environ.get('POSTGRES_USER'), 
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST'),    
+        'PORT': os.environ.get('POSTGRES_PORT')
    }
 }
+
+# DATABASES = {
+#    'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'netflix', 
+#         'USER': 'postgres', 
+#         'PASSWORD': '123',
+#         'HOST': '127.0.0.1',    
+#         'PORT': 5432
+#    }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
